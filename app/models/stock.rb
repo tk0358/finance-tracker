0e8d00cda5615d0f2f92e79d0f905fe7bfa63cd5
tk0358 <tk0358a@yahoo.co.jp>
2019-08-06 06:17:49 +0900

    Add stock restrictions

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,6 +2,10 @@ class Stock < ApplicationRecord
   has_many :user_stocks
   has_many :users, through: :user_stocks
 
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
+
   def self.new_from_lookup(ticker_symbol)
     StockQuote::Stock.new(api_key: 'pk_92a7d680cdee4919b6a3b96d87e4b800')
     begin
@@ -10,10 +14,6 @@ class Stock < ApplicationRecord
     rescue Exception => e
       return nil
     end
-  end
-
-  def self.find_by_ticker(ticker_symbol)
-    where(ticker: ticker_symbol).first
   end
 
 end
