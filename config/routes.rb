52d1ecb12f2a9904839304dc32fd44d5eff11fac
tk0358 <tk0358a@yahoo.co.jp>
2019-08-06 10:32:01 +0900

    Add friendship views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "user/registrations" }
+  resources :user_stocks, only: [:create, :destroy]
+  resources :users, only: [:show]
+  resources :friendships
 
   root 'welcome#index'
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: 'stocks#search'
   get 'my_friends', to: "users#my_friends"
-  resources :user_stocks, only: [:create, :destroy]
+
 end
